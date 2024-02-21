0270b6866098662c4f827a00af84017817a742d7
Pronckunas <pedro_pronckunas@outlook.com>
2024-02-21 20:50:50 -0300

    H2 database, test profile, JPA

--- src/main/java/com/pronckunas/course/entities/User.java
+++ src/main/java/com/pronckunas/course/entities/User.java
@@ -3,9 +3,20 @@ package com.pronckunas.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table (name = "tb_user") //cria uma tabela 'tb_user' com as variáveis de User
 public class User implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //auto-incremento
 	private Long id;
 	private String name;
 	private String email;
