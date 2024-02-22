1db9d8a6b120e60e98a88d9eabfc444e3364bfc9
Pronckunas <pedro_pronckunas@outlook.com>
2024-02-22 18:29:20 -0300

    Service layer, component registration

---
+++ src/main/java/com/pronckunas/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.pronckunas.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pronckunas.course.entities.User;
+import com.pronckunas.course.repositories.UserRepository;
+
+@Service //permite registrar um serviço
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
