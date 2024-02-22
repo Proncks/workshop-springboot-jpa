df8fc705ba4c7c4769a56b6aa2958e4a0487acf4
Pronckunas <pedro_pronckunas@outlook.com>
2024-02-21 21:21:03 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/pronckunas/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.pronckunas.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pronckunas.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+}
