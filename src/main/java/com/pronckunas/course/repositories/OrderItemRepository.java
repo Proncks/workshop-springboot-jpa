71496425c24221f27396a6ef9a26cf8cbddf6c16
Pronckunas <pedro_pronckunas@outlook.com>
2024-02-28 16:55:32 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/pronckunas/course/repositories/OrderItemRepository.java
+++ src/main/java/com/pronckunas/course/repositories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package com.pronckunas.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pronckunas.course.entities.OrderItem;
+import com.pronckunas.course.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
 	
 }
