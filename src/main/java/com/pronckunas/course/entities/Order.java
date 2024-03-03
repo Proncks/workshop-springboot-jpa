6ce08d39f9a03b926e7218d940296b41360c8b18
Pronckunas <pedro_pronckunas@outlook.com>
2024-03-03 17:17:41 -0300

    Subtotal & Total methods

--- src/main/java/com/pronckunas/course/entities/Order.java
+++ src/main/java/com/pronckunas/course/entities/Order.java
@@ -96,10 +96,19 @@ public class Order implements Serializable {
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
 
 	public Set<OrderItem> getItems(){
 		return items;
 	}
+	
 
 	@Override
 	public int hashCode() {
