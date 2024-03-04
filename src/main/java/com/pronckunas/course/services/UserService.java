0db37c55ffa30abdb9822682e03f67658d9ccb84
Pronckunas <pedro_pronckunas@outlook.com>
2024-03-04 11:47:28 -0300

    Exception handling - findById

--- src/main/java/com/pronckunas/course/services/UserService.java
+++ src/main/java/com/pronckunas/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.pronckunas.course.entities.User;
 import com.pronckunas.course.repositories.UserRepository;
+import com.pronckunas.course.services.exceptions.ResourceNotFoundException;
 
 @Service //permite registrar um serviço
 public class UserService {
@@ -20,7 +21,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	//operação para inserir um objeto user no BD
